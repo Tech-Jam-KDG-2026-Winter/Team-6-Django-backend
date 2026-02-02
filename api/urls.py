159614c8afe4160ea02a7877db7656dbf7c0f95b
syumaiking <noguchisyudai0310@gmail.com>
2026-02-02 11:38:47 +0900

    Add login API endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .views import login_view
 
 urlpatterns = [
-    
+    path('api/login/', TokenObtainPairView.as_view()),
+    path("login/", login_view),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
+@csrf_exempt
+def login_view(request):
+    if request.method != "POST":
+        return JsonResponse({"error": "POST only"}, status=405)
+
+    body = json.loads(request.body)
+    username = body.get("username")
+    password = body.get("password")
+
+    return JsonResponse({
+        "message": "login api reached",
+        "username": username
+    })
